0459b8f02333df804a051695f61c60887d0784e6
Ka Ho Cheng <chengy1989@hotmail.com>
2021-10-11 14:45:40 +0900

    fixed the xrandr command, punctuation fixes

--- monitor_check_startup.sh
+++ monitor_check_startup.sh
@@ -1,19 +1,9 @@
 #!/bin/sh
 
-xrandr --listactivemonitors "$monitorcheck"
+monitorcheck='xrandr --listactivemonitors'
 FILE=/etc/X11/xorg.conf.bkp
 
-if [[$monitorcheck == *"Failed to get size of gamma"]]; then
-    if test -f "$FILE"; then
-        sudo mv /etc/X11/xorg.conf.bkp /etc/X11/xorg.conf
-        echo "Switching to Headless mode!, rebooting in 5 seconds"
-        sleep 5s
-        sudo reboot
-    else 
-        echo "No monitor detected,xorg.conf detected Headless mode! already enabled"
-    sleep 5s
-
-else
+if [[$monitorcheck == *"HDMI"]]; then
     if test -f "$FILE"; then
         echo "monitor detected,xorg.conf not detected monitor mode! already enabled"
         sleep 5s
@@ -22,5 +12,17 @@ else
         echo "Monitor detected, Switching to Monitor mode!"
         sleep 5s
         sudo reboot
+    fi
 
+else
+    if test -f "$FILE"; then
+        sudo mv /etc/X11/xorg.conf.bkp /etc/X11/xorg.conf
+        echo "Switching to Headless mode!, rebooting in 5 seconds"
+        sleep 5s
+        sudo reboot
+    else 
+        echo "No monitor detected,xorg.conf detected Headless mode! already enabled"
+    sleep 5s
+    fi
+    
 fi
