d8d80bec074dc0b3bc4a66f455f5e61f84cd7c88
Ka Ho Cheng <chengy1989@hotmail.com>
2021-10-11 19:11:40 +0900

    modified script to run as service

--- monitor_check_startup.sh
+++ monitor_check_startup.sh
@@ -6,24 +6,19 @@ FILE=/etc/X11/xorg.conf.bkp
 if [[$monitorcheck == *"HDMI"]]; then
     if test -f "$FILE"; then
         sudo mv /etc/X11/xorg.conf.bkp /etc/X11/xorg.conf
-        echo "Switching to Headless mode!, rebooting in 5 seconds"
-        sleep 5s
-        sudo reboot
-    else 
-        echo "No monitor detected,xorg.conf detected Headless mode! already enabled"
-    sleep 5s
+        #"Switching to Headless mode!, rebooting in 5 seconds"
+        #sudo reboot
+        #"No monitor detected,xorg.conf detected Headless mode! already enabled"
     fi
     
 
 else
     if test -f "$FILE"; then
-        echo "monitor detected,xorg.conf not detected monitor mode! already enabled"
-        sleep 5s
+        #echo "monitor detected,xorg.conf not detected monitor mode! already enabled"
     else
         sudo mv /etc/X11/xorg.conf /etc/X11/xorg.conf.bkp
-        echo "Monitor detected, Switching to Monitor mode!"
-        sleep 5s
-        sudo reboot
+        #"Monitor detected, Switching to Monitor mode!"
+        #sudo reboot
     fi    
     
 fi
